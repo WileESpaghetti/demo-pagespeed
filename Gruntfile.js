c06a91365763e67f5bd044012aba022b4175daa4
Lehman Black <wileespaghetti@users.noreply.github.com>
2016-11-02 19:56:18 -0500

    Copy files to bin when building

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,23 @@ module.exports = GruntConfig;
 function GruntConfig(grunt) {
 
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
+		pkg: grunt.file.readJSON('package.json'),
+
+		copy: {
+			main: {
+				src: [
+					'**/*.html',
+					'js/**'
+				],
+				expand: true,
+				cwd: 'src',
+				dest: 'bin'
+			}
+		}
 	});
+
+	grunt.registerTask('default', ['copy']);
+
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
 }
