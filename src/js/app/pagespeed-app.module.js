2686049afa0c728852ac2609294752217ce530f3
Lehman Black <wileespaghetti@users.noreply.github.com>
2016-11-05 18:58:13 -0500

    Whitelist pagespeed API endpoint

--- src/js/app/pagespeed-app.module.js
+++ src/js/app/pagespeed-app.module.js
@@ -1,4 +1,8 @@
 (function() {
 	'use strict';
-	angular.module('pagespeedApp', []);
+	angular.module('pagespeedApp', []).config(function($sceDelegateProvider) {
+		$sceDelegateProvider.resourceUrlWhitelist([
+			'https://www.googleapis.com/pagespeedonline/v2/runPagespeed' // TODO pull from service instead of hard coded
+		]);
+	});
 })();
