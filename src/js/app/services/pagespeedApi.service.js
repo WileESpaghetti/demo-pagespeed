d522130b7f963e0cb4194241929315aa350617cd
Lehman Black <wileespaghetti@users.noreply.github.com>
2016-11-05 15:29:01 -0500

    Add service for PageSpeed API

---
+++ src/js/app/services/pagespeedApi.service.js
@@ -0,0 +1,12 @@
+(function() {
+	'use strict';
+	angular.module('pagespeedApp').factory('pagespeedApi', pagespeedApi);
+
+	pagespeedApi.$inject = [];
+
+	function pagespeedApi() {
+		var service = {};
+
+		return service;
+	}
+})();
