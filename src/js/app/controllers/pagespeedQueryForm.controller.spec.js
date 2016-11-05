ec9097da5f520059d36cba52c502bb87d70f2dee
Lehman Black <wileespaghetti@users.noreply.github.com>
2016-11-05 18:22:15 -0500

    Add query form controller test file

---
+++ src/js/app/controllers/pagespeedQueryForm.controller.spec.js
@@ -0,0 +1,8 @@
+describe('Pagespeed Query Form Controller', function() {
+	var pagespeedQueryFormController;
+
+	beforeEach(module('pagespeedApp'));
+	beforeEach(inject(function ($controller, _Messages_) {
+		pagespeedQueryFormController = $controller('pagespeedQueryFormController', {});
+	}));
+});
