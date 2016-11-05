24f5d0151c3bd04989acf3d9e101cad5b9b02fad
Lehman Black <wileespaghetti@users.noreply.github.com>
2016-11-05 18:17:08 -0500

    Add controller for query form

---
+++ src/js/app/controllers/pagespeedQueryForm.controller.js
@@ -0,0 +1,10 @@
+(function() {
+	'use strict';
+	angular.module('pagespeedApp').factory('pagespeedQueryForm', pagespeedQueryForm);
+
+	pagespeedQueryForm.$inject = [];
+
+	function pagespeedQueryForm() {
+		var vm = this;
+	}
+})();
