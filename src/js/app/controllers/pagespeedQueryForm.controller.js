bb8013f953ca762372335fc0ae30c366ea4bd2a0
Lehman Black <wileespaghetti@users.noreply.github.com>
2016-11-05 20:51:00 -0500

    Disable query form submissions when invalid

--- src/js/app/controllers/pagespeedQueryForm.controller.js
+++ src/js/app/controllers/pagespeedQueryForm.controller.js
@@ -9,9 +9,15 @@
 
 		vm.url = '';
 		vm.submit = submit;
+		vm.canNotSubmit = canNotSubmit;
 
 		function submit() {
 			pagespeedApi.run(vm.url);
+		}
+
+		function canNotSubmit() {
+			// On chrome since input[type=url] remains undefined unless value is actually a valid URL
+			return !vm.url; // FIXME also check if API key is set
 		}
 	}
 })();
