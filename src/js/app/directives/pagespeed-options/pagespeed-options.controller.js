3fcd27e5be1e88247c6165b7bb670322ed66d0dd
Lehman Black <wileespaghetti@users.noreply.github.com>
2016-11-12 21:21:51 -0600

    Add directive <pagespeed-options>

---
+++ src/js/app/directives/pagespeed-options/pagespeed-options.controller.js
@@ -0,0 +1,11 @@
+(function() {
+    'use strict';
+
+    angular.module('pagespeedApp').controller('pagespeedOptionsController', pagespeedOptionsController);
+
+    pagespeedOptionsController.$inject = [];
+
+    function pagespeedOptionsController() {
+        var vm = this;
+    }
+})();
---
+++ src/js/app/directives/pagespeed-options/pagespeed-options.directive.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular.module('pagespeedApp').directive('pagespeedOptions', pagespeedOptions);
+
+    function pagespeedOptions() {
+        return {
+            bindToController: true,
+            controller:       'pagespeedOptionsController',
+            controllerAs:     'pagespeedOptionsCtrl',
+            restrict:         'E',
+            scope:            {},
+            templateUrl:      'js/app/directives/pagespeed-options/pagespeed-options.html'
+        };
+    }
+})();
