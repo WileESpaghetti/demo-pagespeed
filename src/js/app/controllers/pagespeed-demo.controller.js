5131539d4495bc34264dd55be223ee4e21fafb3d
Lehman Black <wileespaghetti@users.noreply.github.com>
2016-11-19 15:06:45 -0600

    Add pagespeedDemoController for main view

---
+++ src/js/app/controllers/pagespeed-demo.controller.js
@@ -0,0 +1,10 @@
+(function() {
+	'use strict';
+	angular.module('pagespeedApp').controller('pagespeedDemoController', pagespeedDemoController);
+
+	pagespeedDemoController.$inject = [];
+
+	function pagespeedDemoController() {
+		var vm = this;
+	}
+})();
