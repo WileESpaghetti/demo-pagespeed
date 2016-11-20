977c9fe1b897e8ed3a25eab75c01f60f828319a0
Lehman Black <wileespaghetti@users.noreply.github.com>
2016-11-19 21:53:13 -0600

    Use inline annotation in config block
    Fixes minification issues

--- src/js/app/pagespeed-app.module.js
+++ src/js/app/pagespeed-app.module.js
@@ -1,9 +1,9 @@
 ;(function() {
 	'use strict';
-	angular.module('pagespeedApp', ['pagespeed.templates']).config(function($sceDelegateProvider) {
+	angular.module('pagespeedApp', ['pagespeed.templates']).config(['$sceDelegateProvider', function($sceDelegateProvider) {
 		$sceDelegateProvider.resourceUrlWhitelist([
 			'self',
 			'https://www.googleapis.com/pagespeedonline/v2/runPagespeed' // TODO pull from service instead of hard coded
 		]);
-	});
+	}]);
 })();
