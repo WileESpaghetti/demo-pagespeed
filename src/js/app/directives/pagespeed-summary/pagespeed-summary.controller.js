6c713fc9bdd1d3fcf2a83977e69b85afbf055de4
Lehman Black <wileespaghetti@users.noreply.github.com>
2016-11-19 20:21:06 -0600

    Use cache in summary controller

--- src/js/app/directives/pagespeed-summary/pagespeed-summary.controller.js
+++ src/js/app/directives/pagespeed-summary/pagespeed-summary.controller.js
@@ -3,9 +3,17 @@
 
     angular.module('pagespeedApp').controller('pagespeedSummaryController', pagespeedSummaryController);
 
-    pagespeedSummaryController.$inject = [];
+    pagespeedSummaryController.$inject = ['pagespeedApi'];
 
-    function pagespeedSummaryController() {
-        var vm = this;
+    function pagespeedSummaryController(pagespeedApi) {
+	    var vm = this;
+
+	    vm.pagespeed = pagespeedApi.cache[vm.host];
+
+	    init();
+
+	    function init() {
+		    vm.screenshot = vm.pagespeed.screenshot ? 'data:' + vm.pagespeed.screenshot.mime_type + ';base64,' + vm.pagespeed.screenshot.data.replace(/_/g,'/').replace(/-/g,'+') : 'http://placehold.it/320x240';
+	    }
     }
 })();
